57e460f336f37671d8b370efc98824a2ff679d93
JuanSebastianB <j.becerrab@uniandes.edu.co>
2024-04-20 21:00:51 -0500

    fix: change unit tests

--- app/src/test/java/com/example/inseparables_sportapp_mobile/AutenticacionActivityTest.kt
+++ app/src/test/java/com/example/inseparables_sportapp_mobile/AutenticacionActivityTest.kt
@@ -1,36 +1,43 @@
 package com.example.inseparables_sportapp_mobile
 
-import android.content.Intent
-import android.os.StrictMode
-import android.widget.Button
-import androidx.test.core.app.ActivityScenario
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents.*
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.Before
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Shadows.shadowOf
 import org.robolectric.annotation.Config
+
 
 @RunWith(AndroidJUnit4::class)
 @Config(manifest = Config.NONE)
 class AutenticacionActivityTest {
 
+
+    @get:Rule
+    var activityTestRule = ActivityScenarioRule(
+        AutenticacionActivity::class.java
+    )
+
     @Test
-    fun clickingButton_opensInicioActivity() {
-        // Se inicia la actividad bajo prueba
-        val scenario = ActivityScenario.launch(AutenticacionActivity::class.java)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Assert.assertEquals("com.example.inseparables_sportapp_mobile", appContext.packageName)
+    }
 
-        // Esperar a que la actividad se complete
-        scenario.onActivity { activity ->
-            // Simular el clic en el botón
-            activity.findViewById<Button>(R.id.buttonIngresar).performClick()
-
-            // Verificar que se inició la actividad InicioActivity
-            val expectedIntent = Intent(activity, InicioActivity::class.java)
-            val actualIntent = shadowOf(activity).nextStartedActivity
-            assertThat(actualIntent.toString()).isEqualTo(expectedIntent.toString())
-        }
+    @Test
+    fun ingresarAplicativo() {
+        val targetContext: Context = ApplicationProvider.getApplicationContext()
+        onView(withText(targetContext.resources.getString(R.string.ingresar))).perform(click());
     }
 
 }
