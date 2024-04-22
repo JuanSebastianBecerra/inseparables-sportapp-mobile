ce425ae187d27f0cdf5365c1953d8e52d4f1c0d8
JuanSebastianB <j.becerrab@uniandes.edu.co>
2024-04-21 20:04:29 -0500

    feat: agregar pantalla del listado de entrenamientos

---
+++ app/src/main/java/com/example/inseparables_sportapp_mobile/ui/adapters/EntrenamientoAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.inseparables_sportapp_mobile.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.inseparables_sportapp_mobile.R
+import com.example.inseparables_sportapp_mobile.entities.Entrenamiento
+import com.example.inseparables_sportapp_mobile.ui.viewholders.EntrenamientoViewHolder
+
+class EntrenamientoAdapter(private val entrenamientos: ArrayList<Entrenamiento>) : RecyclerView.Adapter<EntrenamientoViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EntrenamientoViewHolder {
+        return EntrenamientoViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_view_servicio, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return entrenamientos.size
+    }
+
+    override fun onBindViewHolder(holder: EntrenamientoViewHolder, position: Int) {
+        holder.textNombre.text = entrenamientos[position].nombre
+        holder.textLugar.text = entrenamientos[position].lugar
+    }
+}
---
+++ app/src/test/java/com/example/inseparables_sportapp_mobile/EntrenamientosActivityTest.kt
@@ -0,0 +1,21 @@
+package com.example.inseparables_sportapp_mobile
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@RunWith(AndroidJUnit4::class)
+@Config(manifest = Config.NONE)
+class EntrenamientosActivityTest {
+
+    @Test
+    fun testActivityNotNull() {
+        val activityScenario = ActivityScenario.launch(EntrenamientosActivity::class.java)
+        activityScenario.onActivity { activity ->
+            Assert.assertNotNull(activity)
+        }
+    }
+}
