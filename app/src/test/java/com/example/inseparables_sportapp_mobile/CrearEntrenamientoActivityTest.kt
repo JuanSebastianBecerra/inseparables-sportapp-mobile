cfd09cf54952127ecc803fa094dffbcbf0374552
JuanSebastianB <j.becerrab@uniandes.edu.co>
2024-04-24 23:24:53 -0500

    feat: agregar prueba unitaria render

---
+++ app/src/test/java/com/example/inseparables_sportapp_mobile/CrearEntrenamientoActivityTest.kt
@@ -0,0 +1,23 @@
+package com.example.inseparables_sportapp_mobile
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.inseparables_sportapp_mobile.activities.CrearEntrenamientoActivity
+import com.example.inseparables_sportapp_mobile.activities.EntrenamientosActivity
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@RunWith(AndroidJUnit4::class)
+@Config(manifest = Config.NONE)
+class CrearEntrenamientoActivityTest {
+
+    @Test
+    fun testActivityNotNull() {
+        val activityScenario = ActivityScenario.launch(CrearEntrenamientoActivity::class.java)
+        activityScenario.onActivity { activity ->
+            Assert.assertNotNull(activity)
+        }
+    }
+}
