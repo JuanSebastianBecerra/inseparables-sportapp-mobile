a1de3942ff1b0b1fc94dfd364ae670d05de1734b
JuanSebastianB <j.becerrab@uniandes.edu.co>
2024-05-05 21:47:41 -0500

    feat: agregar pruebas unitarias de la lista de eventos

---
+++ app/src/main/java/com/example/inseparables_sportapp_mobile/activities/DetalleEventoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.inseparables_sportapp_mobile.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.inseparables_sportapp_mobile.R
+
+class DetalleEventoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_evento)
+    }
+}
