cc9710167b8a96582bc14baac2dafbc875705194
JuanSebastianB <j.becerrab@uniandes.edu.co>
2024-05-13 18:49:29 -0500

    feat subir cobertura de pruebas unitarias

--- app/src/test/java/com/example/inseparables_sportapp_mobile/DetalleEventoActivityTest.kt
+++ app/src/test/java/com/example/inseparables_sportapp_mobile/DetalleEventoActivityTest.kt
@@ -1,6 +1,11 @@
 package com.example.inseparables_sportapp_mobile
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.inseparables_sportapp_mobile.activities.DetalleEventoActivity
 import com.example.inseparables_sportapp_mobile.activities.DetalleServicioActivity
@@ -37,6 +42,23 @@ class DetalleEventoActivityTest {
             activity.servicios = ArrayList()
             activity.servicios.add(Servicio("","","","","","",""))
             activity.cargarInformacionEvento()
+            activity.llamarDetalleEvento()
+            activity.llamarServicioOrganizador()
+            activity.llamarServiciosPorEvento()
+        }
+    }
+
+    @Test
+    fun hacerClickAgendar(){
+        val activityScenario = ActivityScenario.launch(DetalleEventoActivity::class.java)
+        activityScenario.onActivity { activity ->
+            Assert.assertNotNull(activity)
+            val ubicacion = Ubicacion("", "", "", "", "", "", "")
+            activity.evento = Evento("", "", "", "", "", "", "", ubicacion)
+            activity.socio = Socio("", "", "", "", "", "", "", "")
+            activity.servicios = ArrayList()
+            activity.servicios.add(Servicio("","","","","","",""))
+            activity.botonAgendar.performClick()
         }
     }
 }
