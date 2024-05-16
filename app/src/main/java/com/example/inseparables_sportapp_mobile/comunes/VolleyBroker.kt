836f784fe83fcc3513bb800c652bec5e19b59877
JuanSebastianB <j.becerrab@uniandes.edu.co>
2024-05-15 20:10:53 -0500

    feat: agregar opcion de eliminar un evento de la agenda del deportista

--- app/src/main/java/com/example/inseparables_sportapp_mobile/comunes/VolleyBroker.kt
+++ app/src/main/java/com/example/inseparables_sportapp_mobile/comunes/VolleyBroker.kt
@@ -44,6 +44,18 @@ class VolleyBroker constructor(context: Context) {
             return  jsonObjReq
         }
 
+        fun deleteRequestWithHeaders(path: String, body: JSONObject,  responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener, headersMap: HashMap<String, String> ):JsonObjectRequest{
+            val jsonObjReq: JsonObjectRequest = object : JsonObjectRequest(
+                Method.DELETE, path, body, responseListener, errorListener) {
+                /** Passing some request headers*  */
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): Map<String, String> {
+                    return headersMap
+                }
+            }
+            return  jsonObjReq
+        }
+
     }
 
 }
